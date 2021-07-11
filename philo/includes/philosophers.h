7db0cf789e6b783b51493284ce86e0acd683ffe4
Kenda Shanti <kshanti@si-o4.kzn.21-school.ru>
2021-07-11 21:59:48 +0300

    strarting again

--- philo/includes/philosophers.h
+++ philo/includes/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: kshanti <kshanti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/07 22:26:21 by kshanti           #+#    #+#             */
-/*   Updated: 2021/07/11 19:58:40 by kshanti          ###   ########.fr       */
+/*   Updated: 2021/07/11 20:53:40 by kshanti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,26 +30,15 @@ typedef struct s_settings
 typedef struct s_pthread_philo
 {
 	int						i;
+	int						col_eat;
 	long int				last_eat;
 	long int				limit;
-	pthread_t				pd;
 	struct s_settings		*data;
 	pthread_mutex_t			*left;
 	pthread_mutex_t			*right;
 }							t_pthread_philo;
 /*--------------------------------Utils---------------------------------------*/
-int		my_sleep(int sleep);
-int		free_all(pthread_mutex_t *mutex, t_pthread_philo *philo);
-int		ft_atoi(char *str, char **flag);
-int		ft_strlen(char *str);
-int		error(char *str);
 /*--------------------------------Philo_Utils---------------------------------*/
-void	*life(void	*arg);
 /*--------------------------------Init----------------------------------------*/
-int		get_arg(char *str, int *flag);
-int		init_settings(char **argv, t_settings *tmp);
-void	setting_mutex(t_pthread_philo *philo, pthread_mutex_t *mutex, int i);
-int		go_treads(t_settings *settings, t_pthread_philo **philo, pthread_mutex_t *mutex);
-int		init_mutex(pthread_mutex_t **mutex, int number);
 
 #endif
